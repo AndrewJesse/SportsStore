d10086eefa439cd50bf04e650a5333a792816c67
A.J. Marchese <aj.marchese@outlook.com>
2023-03-17 08:14:51 -0700

    added pagination test

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -15,12 +15,12 @@ namespace SportsStore.Tests
         public void Can_Use_Repository()
         {
             // Arrange
-            Mock<IStoreRepository> mock = new Mock<IStoreRepository>(); 
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
             mock.Setup(m => m.Products).Returns((new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
                 new Product {ProductID = 2, Name = "P2"}
-            }).AsQueryable<Product>()); 
-            
+            }).AsQueryable<Product>());
+
             HomeController controller = new HomeController(mock.Object);
 
             // Act
@@ -31,6 +31,31 @@ namespace SportsStore.Tests
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P1", prodArray[0].Name);
             Assert.Equal("P2", prodArray[1].Name);
+        }
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+            // Act
+            IEnumerable<Product> result =
+                (controller.Index(2) as ViewResult)?.ViewData.Model
+                as IEnumerable<Product> ?? Enumerable.Empty<Product>();
+            // Assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
         }
     }
 }
