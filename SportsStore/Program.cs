ab3d9d5fdcb1be55ef7f43cafded954a94df6e4b
A.J. Marchese <aj.marchese@outlook.com>
2023-03-18 16:02:49 -0700

    added bootstrap and some styling

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -5,12 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); 
 
-builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); }); 
+builder.Services.AddDbContext<StoreDbContext>(opts => { 
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); 
+}); 
+
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>(); 
 
-var app = builder.Build(); 
-app.UseStaticFiles(); 
+var app = builder.Build();
 
+app.UseStaticFiles();
+app.MapControllerRoute("pagination",
+    "Products/Page{productPage}",
+    new { Controller = "Home", action = "Index" });
 app.MapDefaultControllerRoute();
+
 SeedData.EnsurePopulated(app);
+
 app.Run();
