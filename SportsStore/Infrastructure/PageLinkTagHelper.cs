5581e28fac399a6db78ba4c6e139e9a8480e1b8e
A.J. Marchese <aj.marchese@outlook.com>
2023-03-18 17:10:31 -0700

    created partial view for index

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -18,40 +18,33 @@ namespace SportsStore.Infrastructure {
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
-        // This method processes the TagHelperContext and TagHelperOutput for custom pagination functionality.
-        public override void Process(TagHelperContext context,
-            TagHelperOutput output)
+        public bool PageClassesEnabled { get; set; } = false; 
+        public string PageClass { get; set; } = String.Empty; 
+        public string PageClassNormal { get; set; } = String.Empty; 
+        public string PageClassSelected { get; set; } = String.Empty;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            // Ensure both ViewContext and PageModel are not null.
-            if (ViewContext != null && PageModel != null)
-            {
-                // Create an IUrlHelper instance using the urlHelperFactory.
-                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            if (ViewContext != null && PageModel != null) {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext); 
+                TagBuilder result = new TagBuilder("div"); 
+                for (int i = 1; i <= PageModel.TotalPages; i++) 
+                { 
+                    TagBuilder tag = new TagBuilder("a"); 
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i }); 
+                    
+                    if (PageClassesEnabled) 
+                    { 
+                        tag.AddCssClass(PageClass); 
+                        tag.AddCssClass(i == PageModel.CurrentPage 
+                            ? PageClassSelected : PageClassNormal); 
+                    } 
 
-                // Initialize a new TagBuilder for the "div" element.
-                TagBuilder result = new TagBuilder("div");
-
-                // Iterate through each page in the PageModel.
-                for (int i = 1; i <= PageModel.TotalPages; i++)
-                {
-                    // Create a new TagBuilder for the "a" element (anchor tag).
-                    TagBuilder tag = new TagBuilder("a");
-
-                    // Set the "href" attribute of the anchor tag to the appropriate URL.
-                    tag.Attributes["href"] = urlHelper.Action(PageAction,
-                        new { productPage = i });
-
-                    // Set the inner HTML of the anchor tag to the page number.
-                    tag.InnerHtml.Append(i.ToString());
-
-                    // Append the anchor tag to the "div" element.
-                    result.InnerHtml.AppendHtml(tag);
+                    tag.InnerHtml.Append(i.ToString()); 
+                    result.InnerHtml.AppendHtml(tag); 
                 }
-
-                // Set the output content to the generated "div" element.
                 output.Content.AppendHtml(result.InnerHtml);
             }
         }
-
     }
 }
