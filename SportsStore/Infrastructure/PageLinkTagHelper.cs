11ba55b28201f36b446cc53367aa8fb0afe0cb21
A.J. Marchese <aj.marchese@outlook.com>
2023-03-21 08:28:06 -0700

    added navigation component and test

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -18,6 +18,8 @@ namespace SportsStore.Infrastructure {
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")] public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
+
         public bool PageClassesEnabled { get; set; } = false; 
         public string PageClass { get; set; } = String.Empty; 
         public string PageClassNormal { get; set; } = String.Empty; 
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
 
@@ -14,9 +15,19 @@ builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.MapControllerRoute("pagination",
-    "Products/Page{productPage}",
+
+app.MapControllerRoute("catpage","{category}/Page{productPage:int}", 
     new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute("page", "Page{productPage:int}", 
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("category", "{category}", 
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("pagination", "Products/Page{productPage}", 
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapDefaultControllerRoute();
 
 SeedData.EnsurePopulated(app);
