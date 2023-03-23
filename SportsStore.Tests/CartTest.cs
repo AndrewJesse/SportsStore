44d4ad7e818de296e988cf6f16c8db28095887b5
A.J. Marchese <aj.marchese@outlook.com>
2023-03-23 08:23:20 -0700

    finished clear shopping cart test

--- SportsStore.Tests/CartTest.cs
+++ SportsStore.Tests/CartTest.cs
@@ -90,5 +90,24 @@ namespace SportsStore.Tests
             // Assert
             Assert.Equal(450M, result);
         }
-    }
+        [Fact]
+        public void Can_Clear_Contents()
+        { 
+            // Arrange - create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M }; 
+            
+            // Arrange - create a new cart
+            Cart target = new Cart(); 
+            
+            // Arrange - add some items
+            target.AddItem(p1, 1); 
+            target.AddItem(p2, 1); 
+            
+            // Act - reset the cart
+            target.Clear(); 
+            
+            // Assert
+            Assert.Empty(target.Lines); }
+        }
 }
