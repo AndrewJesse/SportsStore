d7291f7ab1ed6a3ca2fd36a9de36b15e02e8cf3b
A.J. Marchese <aj.marchese@outlook.com>
2023-03-24 09:26:03 -0700

    added virtual keyword to cart class

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -3,7 +3,7 @@
     public class Cart 
     { 
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
-        public void AddItem(Product product, int quantity) { 
+        public virtual void AddItem(Product product, int quantity) { 
             CartLine? line = Lines
                 .Where(p => p.Product.ProductID == product.ProductID)
                 .FirstOrDefault(); 
@@ -17,11 +17,11 @@
                 line.Quantity += quantity;
             }
         }
-        public void RemoveLine(Product product) 
+        public virtual void RemoveLine(Product product) 
             => Lines.RemoveAll(l => l.Product.ProductID == product.ProductID); 
         public decimal ComputeTotalValue() 
             => Lines.Sum(e => e.Product.Price * e.Quantity); 
-        public void Clear() => Lines.Clear();
+        public virtual void Clear() => Lines.Clear();
     }
     public class CartLine 
     { 
