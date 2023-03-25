97ae508f04eb75ae16b97ab21f1cb1ca8e5e4cab
A.J. Marchese <aj.marchese@outlook.com>
2023-03-25 11:53:50 -0700

    added checkout view and view controller

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Controllers 
+{ 
+    public class OrderController : Controller 
+    { 
+        public ViewResult Checkout() => View(new Order()); 
+    } 
+}
