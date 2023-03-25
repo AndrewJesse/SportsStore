4ac756d1d3ddf6d6a79ec696d85c22e4b7898543
A.J. Marchese <aj.marchese@outlook.com>
2023-03-24 21:46:46 -0700

    added shopping cart feature

--- SportsStore/Pages/Cart.cshtml.cs
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -6,27 +6,31 @@ namespace SportsStore.Pages
 { 
     public class CartModel : PageModel 
     { 
-        private IStoreRepository repository; 
-        public CartModel(IStoreRepository repo) 
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repo, Cart cartService) 
         { 
             repository = repo; 
+            Cart = cartService; 
         } 
-        public Cart? Cart { get; set; } 
+        public Cart Cart { get; set; } 
         public string ReturnUrl { get; set; } = "/"; 
         public void OnGet(string returnUrl) 
         { 
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
-        }
+        } 
         public IActionResult OnPost(long productId, string returnUrl) 
         { 
-            Product? product = repository.Products.
-                FirstOrDefault(p => p.ProductID == productId); 
+            Product? product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId); 
             if (product != null) 
             { 
-                Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart(); 
-                Cart.AddItem(product, 1); HttpContext.Session.SetJson("cart", Cart); 
-            } 
+                Cart.AddItem(product, 1); 
+            }
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+        public IActionResult OnPostRemove(long productId, string returnUrl) 
+        { 
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == productId).Product); 
             return RedirectToPage(new { returnUrl = returnUrl }); 
         }
     }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -5,13 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); 
 
-builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); }); 
+builder.Services.AddDbContext<StoreDbContext>(opts => { 
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:SportsStoreConnection"]); 
+}); 
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>(); 
 
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache(); 
 builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp)); 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build(); 
 
